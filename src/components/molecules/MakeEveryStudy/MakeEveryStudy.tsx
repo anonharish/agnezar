ed59d8edba62267c9f751044e64ffe02f3e59f86
Harish.K <kollatiharish@gmail.com>
2025-10-24 14:54:23 +0530

    formatted the page

--- src/components/molecules/MakeEveryStudy/MakeEveryStudy.tsx
+++ src/components/molecules/MakeEveryStudy/MakeEveryStudy.tsx
@@ -7,53 +7,55 @@ import { CustomFilledButton } from '@components/atoms';
 import UnParalleled from '../UnParalleled/UnParalleled';
 
 export const MakeEveryStudy: React.FC = () => {
-  const { menu } = useMakeEveryStudy();
-  const [activeMenu, setActiveMenu] = useState(menu[0].id);
+    const { menu } = useMakeEveryStudy();
+    const [activeMenu, setActiveMenu] = useState(menu[0].id);
 
-  return (
-    <StyledMakeEvery>
-      <UnParalleled/>  
-      <Inner>
-        <VerticalMenu>
-          {menu.map(m => (
-            <MenuItem key={m.id} className={m.id === activeMenu ? 'active' : ''} onClick={() => setActiveMenu(m.id)}>{m.label}</MenuItem>
-          ))}
-        </VerticalMenu>
+    return (
+        <StyledMakeEvery>
+            <UnParalleled />
+            <Inner>
+                <VerticalMenu>
+                    {menu.map(m => (
+                        <MenuItem key={m.id} className={m.id === activeMenu ? 'active' : ''} onClick={() => setActiveMenu(m.id)}>{m.label}</MenuItem>
+                    ))}
+                </VerticalMenu>
 
-        <AccordionsWrap>
-          {menu.find(m => m.id === activeMenu)?.items.map(item => (
-            <Accordion
-              key={item.id}
-              sx={{
-                boxShadow: 'none',
-                backgroundColor: theme => (theme.palette.background?.default ?? '#fff'),
-                '&:before': { display: 'none' },
-              }}
-            >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                sx={{
-                  py: 2,
-                  
-                  '& .MuiAccordionSummary-content': { margin: 0 },
-                  borderBottom: theme => `1px solid ${theme.palette.divider}`,
-                  
-                }}
-              >
-                <Typography variant="h6" sx={{ fontWeight: 600,
-                  fontSize: "1.5rem", }}>{item.title}</Typography>
-              </AccordionSummary>
+                <AccordionsWrap>
+                    {menu.find(m => m.id === activeMenu)?.items.map(item => (
+                        <Accordion
+                            key={item.id}
+                            sx={{
+                                boxShadow: 'none',
+                                backgroundColor: theme => (theme.palette.background?.default ?? '#fff'),
+                                '&:before': { display: 'none' },
+                            }}
+                        >
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                sx={{
+                                    py: 2,
 
-              <AccordionDetails sx={{ pt: 2, pb: 4, backgroundColor: theme => (theme.palette.background?.default ?? '#fff') , borderBottom: theme => `1px solid ${theme.palette.divider}`,}}>
-                <Typography variant="body1" sx={{ mb: 3, fontSize: "1rem", color: theme => theme.palette.text.secondary }}>{item.summary}</Typography>
-                <CustomFilledButton onClick={() => {}}>{item.buttonLabel}</CustomFilledButton>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </AccordionsWrap>
-      </Inner>
-    </StyledMakeEvery>
-  );
+                                    '& .MuiAccordionSummary-content': { margin: 0 },
+                                    borderBottom: theme => `1px solid ${theme.palette.divider}`,
+
+                                }}
+                            >
+                                <Typography variant="h6" sx={{
+                                    fontWeight: 600,
+                                    fontSize: "1.5rem",
+                                }}>{item.title}</Typography>
+                            </AccordionSummary>
+
+                            <AccordionDetails sx={{ pt: 2, pb: 4, backgroundColor: theme => (theme.palette.background?.default ?? '#fff'), borderBottom: theme => `1px solid ${theme.palette.divider}`, }}>
+                                <Typography variant="body1" sx={{ mb: 3, fontSize: "1rem", color: theme => theme.palette.text.secondary }}>{item.summary}</Typography>
+                                <CustomFilledButton onClick={() => { }}>{item.buttonLabel}</CustomFilledButton>
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+                </AccordionsWrap>
+            </Inner>
+        </StyledMakeEvery>
+    );
 };
 
 export default MakeEveryStudy;
